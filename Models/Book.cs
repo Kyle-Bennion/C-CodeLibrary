f07f74dfed47fdfc215c99a33167334a2a01f413
Kyle Bennion <bilekennion@hotmail.com>
2020-10-13 15:11:23 -0600

    Book.cs is almost complete

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,6 +1,19 @@
-using System;
-
 namespace CodeLibrary.Models
 {
-  class Book
+  public class Book
   {
+
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Description { get; set; }
+    public bool IsAvailable { get; set; }
+    public Book(string title, string author, string description, bool isAvailable)
+    {
+      Title = title;
+      Author = author;
+      Description = description;
+      IsAvailable = true;
+    }
+  }
+
+}
